572b791aed2659110b5eded1a51b8e6d0f143736
wertkiz <wertkiz@DESKTOP-N39HHBO>
2024-09-24 16:42:20 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(402, 58);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 486);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(443, 525);
+            this.MinimumSize = new System.Drawing.Size(443, 525);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Часы";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        int ugolSec = -90;
+        int ugolMin = -90;
+        int ugolHour = -90;
+        SolidBrush SB = new SolidBrush(SystemColors.Control);
+        Bitmap bmp = new Bitmap(400, 400);
+        Graphics gr;
+        float centr = 200;
+
+        Pen PenTabl = new Pen(Color.Black, 4f);
+
+        #region секунды
+        int lengSec = 165;
+        Pen PenSec = new Pen(Color.Black,2f);
+        #endregion
+
+        #region минуты
+        int lengMin = 120;
+        Pen PenMin = new Pen(Color.Black, 1f);
+        #endregion
+
+        #region Часы
+        int lengHour = 80;
+        Pen PenHour = new Pen(Color.Black, 3f);
+        #endregion
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            gr = Graphics.FromImage(bmp);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            #region Очистка Picturbox1
+            gr.FillRectangle(SB, new Rectangle(0, 0, 400, 400));
+            #endregion
+
+            #region рисовка циферблата
+
+            gr.DrawLine(PenTabl, 200, 0, 200, 30);
+            gr.DrawLine(PenTabl, 200, 370, 200, 400);
+
+            gr.DrawLine(PenTabl, 0, 200, 30, 200);
+            gr.DrawLine(PenTabl, 370, 200, 400, 200);
+
+            #endregion
+
+            #region Часы
+            ugolHour = (DateTime.Now.Hour * 30) - 90;
+            gr.DrawLine(PenHour, centr, centr, x2(lengHour, ugolHour), y2(lengHour, ugolHour));
+            #endregion
+
+            #region Минуты
+            ugolMin = (DateTime.Now.Minute * 6) - 90;
+            gr.DrawLine(PenMin, centr, centr, x2(lengMin, ugolMin), y2(lengMin, ugolMin));
+            #endregion
+
+            #region Секунды
+            ugolSec = (DateTime.Now.Second * 6) - 90;
+            gr.DrawLine(PenSec, centr, centr, x2(lengSec,ugolSec),y2(lengSec, ugolSec));
+            #endregion
+
+            pictureBox1.Image = bmp;
+            label1.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private float x2(int len, int ug)
+        {
+            return centr + len * (float)(Math.Cos(Math.PI * ug / 180));
+        }
+
+        private float y2(int len, int ug)
+        {
+            return centr + len * (float)(Math.Sin(Math.PI * ug / 180));
+        }
+    }
+}
